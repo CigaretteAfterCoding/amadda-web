53b0c8d3b1d2136586d3c432c00035032392a570
Bonggeun Jeong <jungcome7@gmail.com>
2022-01-02 12:14:31 +0900

    fix: something changed

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 import Link from 'next/link';
 
 function HomePage() {
-  return (<div>Welcome to Next.js!
-    <Link href="/cc">
-    adfadfddsafadsf
-    </Link>
-  </div>);
+  return (
+    <div>Welcome to Next.js!
+      <Link href="/cc">
+        adfadfddsafadsfdsaf
+      </Link>
+    </div>
+  );
 }
 
 export default HomePage;
