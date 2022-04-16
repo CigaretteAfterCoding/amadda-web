8c3f56fbd9164ae96c10254b9f4c691f0dedcf27
flex-bonggeun <jungcome7@gmail.com>
2022-04-16 18:58:21 +0900

    fifi

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,21 +1,22 @@
-import React from 'react';
-import { AppProps } from 'next/app';
-import { QueryClient, QueryClientProvider } from 'react-query';
-import { ReactQueryDevtools } from 'react-query/devtools';
-import { Global } from '@emotion/react';
-import { baseCSS } from 'amadda-ui';
+import { AppProps } from "next/app";
+import { ReactNode } from "react";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
+
+declare module "react-query/types/react/QueryClientProvider" {
+  interface QueryClientProviderProps {
+    children?: ReactNode;
+  }
+}
 
 const queryClient = new QueryClient();
 
 function AmaddaApp({ Component, pageProps }: AppProps) {
   return (
-    <>
-      <QueryClientProvider client={queryClient}>
-        <ReactQueryDevtools initialIsOpen={false} />
-        <Global styles={baseCSS} />
-        <Component {...pageProps} />
-      </QueryClientProvider>
-    </>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools initialIsOpen={false} />
+      <Component {...pageProps} />
+    </QueryClientProvider>
   );
 }
 
