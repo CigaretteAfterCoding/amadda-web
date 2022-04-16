c31616cd7611767a9e727ec6bea6de39833af6a4
flex-bonggeun <jungcome7@gmail.com>
2022-04-16 19:28:22 +0900

    env: setup

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,11 @@
-import React from 'react';
-import Link from 'next/link';
+import Link from "next/link";
 
 function HomePage() {
   return (
     <div>
       Welcome to Amadda
-      <Link href="/sign-up">
-        회원가입
-      </Link>
-      <Link href="/sign-in">
-        로그인
-      </Link>
+      <Link href="/sign-up">회원가입</Link>
+      <Link href="/sign-in">로그인</Link>
     </div>
   );
 }
